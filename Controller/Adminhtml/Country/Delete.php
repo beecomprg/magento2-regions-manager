376144a140e4875764898d50066d2c262642a546
Lukas Beranek <lukas@beecom.io>
2020-03-14 01:13:50 +0100

    init

---
+++ Controller/Adminhtml/Country/Delete.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ *
+ * @Author              Ngo Quang Cuong <bestearnmoney87@gmail.com>
+ * @Date                2016-12-12 21:47:12
+ * @Last modified by:   nquangcuong
+ * @Last Modified time: 2016-12-12 22:22:47
+ */
+
+namespace Beecom\Country\Controller\Adminhtml\Country;
+
+class Delete extends \Magento\Backend\App\Action
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Beecom_Country::region_delete';
+    /**
+     *
+     * @return \Magento\Framework\View\Result\PageFactory
+     */
+    public function execute()
+    {
+        // check if we know what should be deleted
+        $region_id = $this->getRequest()->getParam('region_id');
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if ($region_id) {
+            $region_name = '';
+            try {
+                // init model and delete
+                $model = $this->_objectManager->create('Beecom\Country\Model\Country');
+                $model->load($region_id);
+                $region_name = $model->getDefaultName();
+                $model->delete();
+                $this->messageManager->addSuccess(__('The '.$region_name.' Country has been deleted.'));
+                return $resultRedirect->setPath('*/*/');
+            } catch (\Exception $e) {
+                // display error message
+                $this->messageManager->addError($e->getMessage());
+                // go back to edit form
+                return $resultRedirect->setPath('*/*/edit', ['region_id' => $region_id]);
+            }
+        }
+        // display error message
+        $this->messageManager->addError(__('Country to delete was not found.'));
+        // go to grid
+        return $resultRedirect->setPath('*/*/');
+    }
+}
---
+++ Controller/Adminhtml/Country/Index.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *
+ * @Author              Ngo Quang Cuong <bestearnmoney87@gmail.com>
+ * @Date                2016-12-11 21:02:32
+ * @Last modified by:   nquangcuong
+ * @Last Modified time: 2016-12-13 16:00:34
+ */
+
+namespace Beecom\Country\Controller\Adminhtml\Country;
+
+class Index extends \Beecom\Country\Controller\Adminhtml\Country
+{
+    /**
+     * @return \Magento\Framework\View\Result\PageFactory
+     */
+    public function execute()
+    {
+        $resultPage = $this->_initAction();
+        $resultPage->addBreadcrumb(
+            'Countrys Manager',
+            'Countrys Manager'
+        );
+        $resultPage->getConfig()->getTitle()->prepend(__('Customers'));
+        $resultPage->getConfig()->getTitle()
+            ->prepend('Countrys Manager');
+        return $resultPage;
+    }
+}
